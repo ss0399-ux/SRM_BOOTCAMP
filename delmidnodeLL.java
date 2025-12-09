feeb14e9e6eda14011767aac317b6e15ab150a96
ss0399-ux <ss0399@srmist.edu.in>
2025-12-09 14:04:15 +0530

    delmidnodeLL.java

---
+++ delmidnodeLL.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        // If only one node, return null
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        // Move fast by 2 steps and slow by 1 step
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Delete the middle node
+        prev.next = slow.next;
+
+        return head;
+    }
+}
