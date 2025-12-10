4ff45e6edd23a7aaff5ef73303b6df325c113164
ss0399-ux <ss0399@srmist.edu.in>
2025-12-10 11:37:37 +0530

    Update IntersectionLL.java

--- IntersectionLL.java
+++ IntersectionLL.java
@@ -1,4 +1,4 @@
-public class Solution {
+/*public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode temp1 = headA;
         ListNode temp2 = headB;
@@ -32,4 +32,18 @@ public class Solution {
             return null;
         }
         
+}*/
+
+ optimal approach 
+    public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        while(temp1 != temp2){
+            temp1 = (temp1 == null)? headB : temp1.next;
+            temp2 = (temp2 == null)? headA : temp2.next;
+        }
+        return temp1;
+        
+    }
 }
