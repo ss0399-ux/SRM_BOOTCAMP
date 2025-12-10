d35bd8ff84b6e8535fc1e61adc5efa4950fe2035
ss0399-ux <ss0399@srmist.edu.in>
2025-12-10 09:58:03 +0530

    reverseLL2.java

---
+++ reverseLL2.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode leftPre = dummy;
+        ListNode currNode = head;
+
+        for(int i = 0; i<left-1; i++){
+            leftPre = leftPre.next;
+            currNode  = currNode.next;
+        }
+
+        ListNode subListHead = currNode;
+        ListNode prevNode = null;
+
+        for(int i= 0; i<= right-left; i++){
+            ListNode nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        leftPre.next = prevNode;
+        subListHead.next = currNode;
+        
+        return dummy.next;
+    }
+}
