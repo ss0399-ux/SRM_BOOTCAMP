6ee925cf4f506b96818186d2773752dac3de910b
ss0399-ux <ss0399@srmist.edu.in>
2025-12-10 11:20:52 +0530

    IntersectionLL.java

---
+++ IntersectionLL.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        int n1 = 0;
+        int n2 = 0;
+
+        while(temp1 != null){
+            n1++;
+            temp1 = temp1.next;
+        }
+        while(temp2 !=null){
+            n2++;
+            temp2 = temp2.next;
+        }
+        if(n1 < n2){
+            return findIntersection(headA, headB, n2-n1);
+        }else{
+            return findIntersection(headB, headA, n1-n2);
+        }
+    }
+
+        private ListNode findIntersection(ListNode shorter, ListNode longer, int diff){
+            while(diff --> 0){
+                longer = longer.next;
+            }
+            while(shorter != null && longer != null){
+                if(shorter == longer) return shorter;
+                shorter = shorter.next;
+                longer = longer.next;  
+            }
+            return null;
+        }
+        
+}
